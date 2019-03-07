68d429cd7d89941371fa2e8f36459cd030057d74
Joel Bartlett <joel@murbar.com>
2019-03-07 17:31:04 -0500

    Add authService

---
+++ friends/src/services/authService.js
@@ -0,0 +1,25 @@
+import http from './httpService';
+
+const apiEndpoint = 'http://localhost:5000/api/login';
+const tokenKey = 'token';
+
+http.setToken(getCurrentUser());
+
+export async function login(username, password) {
+  const { data: token } = await http.post(apiEndpoint, { username, password });
+  localStorage.setItem(tokenKey, token);
+}
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  return localStorage.getItem(tokenKey);
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser
+};
