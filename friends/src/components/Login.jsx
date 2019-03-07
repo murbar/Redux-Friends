8c8588da056928553c64fbbf4a99da2a4c508629
Joel Bartlett <joel@murbar.com>
2019-03-07 16:25:11 -0500

    Stub out login form

---
+++ friends/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
