6da17896a0c4dbdb4d0596e27c52c7fe15162976
Joel Bartlett <joel@murbar.com>
2019-03-07 19:06:55 -0500

    Add login form state

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  //  if logged in, redirect to FriendsList
+  const [credentials, setCredentials] = useState({});
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setCredentials(values => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
-      <form>
-        <input type="text" />
-        <input type="password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username || ''}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password || ''}
+          onChange={handleChange}
+          placeholder="Password"
+        />
         <button>Login</button>
       </form>
     </div>
