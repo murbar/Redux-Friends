a4c9020d953bfb820ab50294c6d6a90236bac7ca
Joel Bartlett <joel@murbar.com>
2019-03-07 20:55:23 -0500

    Redirect to login route

--- friends/src/components/ProtectedRoute.jsx
+++ friends/src/components/ProtectedRoute.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import auth from '../services/authService';
 
-const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+const ProtectedRoute = ({ component: Component, render, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props => {
-        if (!auth.getCurrentUser()) return <Redirect to="/" />;
+        if (!auth.getCurrentUser()) return <Redirect to="/login" />;
         return Component ? <Component {...props} /> : render(props);
       }}
     />
