81324a2055f2de7fc608fc8f1dea813285af6a6e
Joel Bartlett <joel@murbar.com>
2019-03-07 21:28:10 -0500

    Add nav links

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -4,13 +4,32 @@ import './App.css';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import FriendsList from './components/FriendsList';
+import Logout from './components/Logout';
 
 const App = () => {
   return (
     <Router>
       <main>
-        <Route path="/" component={Login} />
-        <ProtectedRoute exact path="friends-list" component={FriendsList} />
+        <h1>Redux With Friends</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/friends-list">Friends List</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/login" component={Login} />
+        <ProtectedRoute path="/friends-list" component={FriendsList} />
+        <Route path="/logout" component={Logout} />
       </main>
     </Router>
   );
